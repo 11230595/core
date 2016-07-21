a91c176783f67a5e7f039022aa619743c92f5344
zhoudong <11230595@qq.com>
2016-07-21 09:56:58 +0800

    rabbitmq

--- core/src/main/java/com/hexun/framework/core/rabbitmq/RabbitmqConfig.java
+++ core/src/main/java/com/hexun/framework/core/rabbitmq/RabbitmqConfig.java
@@ -3,6 +3,7 @@ package com.hexun.framework.core.rabbitmq;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import com.hexun.framework.core.properties.RabbitmqPropertiesUtils;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -26,10 +27,15 @@ public final class RabbitmqConfig{
 	public static Channel getReqvChannel() throws IOException, TimeoutException{
 		if(factory == null){
 			factory = new ConnectionFactory();
-			factory.setHost("10.0.202.152");
+			/*factory.setHost("10.0.202.152");
 			factory.setPort(5672);
 			factory.setUsername("admin");
-			factory.setPassword("admin");
+			factory.setPassword("admin");*/
+			
+			factory.setHost(RabbitmqPropertiesUtils.getString("rabbitmq.ip"));
+			factory.setPort(RabbitmqPropertiesUtils.getInt("rabbitmq.port"));
+			factory.setUsername(RabbitmqPropertiesUtils.getString("rabbitmq.username"));
+			factory.setPassword(RabbitmqPropertiesUtils.getString("rabbitmq.password"));
 		}
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
